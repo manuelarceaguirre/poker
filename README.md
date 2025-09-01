193e35fca45a966d3fdc8b6310e11a84299a7ad1
Manuel Arce <manuel.arceaguirre@gmail.com>
2025-08-31 19:42:15 -0700

    evaluation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # poker
+Straight Flush -> 8
+Four of a Kind -> 7
+Full House -> 6
+Flush -> 5
+Straight -> 4
+Three of a Kind -> 3
+Two Pair -> 2
+One Pair -> 1
+High Card -> 0
--- draft.py
+++ draft.py
@@ -1,4 +1,5 @@
 import random
+from collections import Counter
 cards = list(range(52))
 
 # helper functions
@@ -11,13 +12,26 @@ def decode_card(card_number):
     suit = suits[card_number // 13]
     return f'{rank} of {suit}'
 
+def decode_card_tuple(card_number):
+    rank = card_number % 13
+    suit = card_number // 13
+    return (rank, suit)
+
+def decode_hand(strongest_num):
+    hands = ["High Card", "One Pair", "Two Pair", "Three of a kind", "Straight", "Flush", "Full House", "Four of a kind", "Straight Flush"]
+    hand = hands[strongest_num]
+    return hand
+
 class Player():
-    def __init__(self, name, money, hand = None, decoded_hand = None):
+    def __init__(self, name, money, hand = None, decoded_hand = None, shared_hand = None, strongest_hand = None):
         self.name = name
         self.money = money
         self.hand = hand if hand is not None else []
         self.decoded_hand = decoded_hand if decoded_hand is not None else []
-    
+        self.shared_hand = shared_hand if shared_hand is not None else []
+        self.decoded_shared_hand = shared_hand if shared_hand is not None else []
+        self.strongest_hand = strongest_hand if strongest_hand is not None else 0
+
     def show_hand(self):
         return f'{self.name} has {self.decoded_hand}'
 
@@ -139,17 +153,96 @@ class Table():
         self.phase = 'showdown'
     
     def showdown(self):
+        print(self.phase)       
         # guardrails
         if self.phase != 'showdown':
             raise Exception('Not a showdown yet')
         
-        # TODO WINNING LOGIC
-        # show the upcards and players cards
-        for player in self.players:
-            print(player.show_hand())
-        print(self.show_up_cards())
-
+        # WINNING LOGIC
         
+        # show own, shared, community hands
+        for player in self.players:
+            _temp_list = player.hand + self.upcards
+            for card in _temp_list:
+                # print string
+                decoded = decode_card(card)
+                player.decoded_shared_hand.append(decoded)
+                # logic tuples
+                decoded = decode_card_tuple(card)
+                player.shared_hand.append(decoded)
+            
+            print(f'own hand {player.show_hand()}')
+            print(f'shared hand {player.decoded_shared_hand}')
+            print(f'logic {player.shared_hand}')
+            
+            # rank counts 
+            ranks = []
+            for pair in player.shared_hand:
+                ranks.append(pair[0])
+            ranks_counts = Counter(ranks)
+            print(ranks_counts)
+
+            # suit counts
+            suits = []
+            for pair in player.shared_hand:
+                suits.append(pair[1])
+            suits_counts = Counter(suits)
+            print(suits_counts)
+
+            # sort ranks
+            ranks = list(set(sorted(ranks)))
+
+            # evaluate ranks
+            full_house_flag = 0
+            two_pair_flag = 0 
+            for rank, frequency in ranks_counts.items():
+                if frequency == 4:
+                    player.strongest_hand = max(player.strongest_hand, )
+                elif frequency == 3:
+                    if full_house_flag == 1:
+                        player.strongest_hand = max(player.strongest_hand, 6)
+                    else:
+                        full_house_flag += 1
+                        player.strongest_hand = max(player.strongest_hand, 3)
+                elif frequency == 2:
+                    if full_house_flag == 1:
+                        player.strongest_hand = max(player.strongest_hand, 6)
+                    elif two_pair_flag == 1:
+                        player.strongest_hand = max(player.strongest_hand, 2)
+                    else:
+                        two_pair_flag = 1
+                        player.strongest_hand = max(player.strongest_hand, 1)
+
+            # evaluate suits
+            for suits, frequency in suits_counts.items():
+                if frequency >= 5:
+                    player.strongest_hand = max(player.strongest_hand, 5)
+
+            # evaluate sorted ranks
+            consecutive_flag = 1
+            for i in range(1,len(ranks)):
+                if i > 4:
+                    if ranks[i] == 0 and ranks[i-1] == 12 and ranks[i-2] == 11 and ranks[i-3] == 10 and ranks[i-4] == 9:
+                        player.strongest_hand = max(player.strongest_hand, 4)
+                
+                diff = ranks[i] - ranks[i-1]
+                if diff == 0:
+                    continue
+                elif diff == 1:
+                    consecutive_flag += 1
+                else:
+                    consecutive_flag = 1
+                
+                if consecutive_flag == 5:
+                    player.strongest_hand = max(player.strongest_hand, 4)
+
+            print(f'Player {player.name} has {player.strongest_hand}')
+            decoded = decode_hand(player.strongest_hand)
+            print(f'Player {player.name} has {decoded}')
+
+
+        # show community cards
+        print(f'community cards: {self.show_up_cards()}')
 
 
         # TODO MONEY TRANSFERS
